e455010b4fe56f257f223f938fce2998aa0c153b
Kiran Challa <kichalla@microsoft.com>
2016-03-21 11:13:56 -0700

    Repro code

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using StackExchange.Redis;
+
+namespace ConsoleApplication
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            var connectionMultiplexer = ConnectionMultiplexer.Connect("localhost:6379");
+            var database = connectionMultiplexer.GetDatabase();
+            Console.WriteLine("Done!");
+        }
+    }
+}
